269f355c9497db7d3e484eca402ce8e01164ffcd
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-06-16 14:49:49 +0200

    LoginView

---
+++ ManicaChunksOnline/UICommon/RoundButton.swift
@@ -0,0 +1,36 @@
+//
+//  RoundButton.swift
+//  ManicaChunksOnline
+//
+//  Created by Manyuchi, Carrington C on 2026/06/16.
+//
+
+
+import SwiftUI
+
+struct RoundButton: View {
+    
+    @State var title: String = "title"
+    @State var backgroundColor: Color = .primaryApp
+    var didTap: (() -> ())?
+    
+    var body: some View {
+        Button {
+            didTap?()
+        } label: {
+            Text(title)
+                .font(.gilroyCustomFont(.semibold, fontSize: 18))
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+        .background(backgroundColor)
+        .cornerRadius(20)
+
+    }
+}
+
+#Preview {
+    RoundButton(title: "Get started Noe")
+        .padding(20)
+}
--- ManicaChunksOnline/Views/Login/LoginView.swift
+++ ManicaChunksOnline/Views/Login/LoginView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct LoginView: View {
+    @Environment(\.dismiss) var dismiss
+    @State var emailText: String = ""
+    @State var passwordText: String = ""
+    @State var isShowPassword: Bool = false
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -16,13 +21,12 @@ struct LoginView: View {
                     .scaledToFill()
                     .frame(width: geometry.size.width, height: geometry.size.height)
                 
-                
                 VStack {
                     Image("color_logo")
                         .resizable()
                         .scaledToFit()
                         .frame(height: 40)
-                        .padding(.bottom, 30)
+                        .padding(.bottom, geometry.size.height * 3/100)
                     
                     Text("Login")
                         .font(.gilroyCustomFont(.semibold, fontSize: 26))
@@ -32,11 +36,74 @@ struct LoginView: View {
                     Text("Enter your email and password")
                         .font(.gilroyCustomFont(.semibold, fontSize: 16))
                         .foregroundStyle(Color.secondaryText)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, geometry.size.height * 3/100)
                     
+                    LineTextField(
+                        title: "Email",
+                        placeholder: "Enter your email address",
+                        txt: $emailText,
+                        keyboardType: .emailAddress
+                    )
+                    .padding(.bottom, geometry.size.height * 3/100)
                     
-                    
-                    
+                    LineSecureField(
+                        title: "Password",
+                        placeholder: "Enter your password",
+                        txt: $passwordText,
+                        isShowPassword: $isShowPassword
+                    )
+                    .padding(.bottom, geometry.size.height * 1/100)
+
+                    Button {
+                        
+                    } label: {
+                        Text("Forgot Password")
+                            .font(.gilroyCustomFont(.medium, fontSize: 14))
+                            .foregroundStyle(Color.primaryText)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                    .padding(.bottom, geometry.size.height * 3/100)
+
+                    RoundButton(title: "Log In") {
+                        
+                    }
+                    .padding(.bottom, geometry.size.height * 1.5/100)
+
+                    NavigationLink {
+                        
+                    } label: {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.gilroyCustomFont(.semibold, fontSize: 14))
+                                .foregroundStyle(Color.primaryText)
+                            
+                            Text("Signup")
+                                .font(.gilroyCustomFont(.semibold, fontSize: 14))
+                                .foregroundStyle(Color.primaryApp)
+                        }
+                    }
+                    Spacer()
                 }
+                .padding(.top, geometry.size.height * 18/100)
+                .padding(.horizontal)
+                
+                VStack {
+                    HStack {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image("back")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, geometry.size.height * 8/100)
             }
         }
         .ignoresSafeArea()
